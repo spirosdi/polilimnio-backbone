4a037cd97eadce37f22509e17d6e4c33702e9548
Spiros Dimopulos <spiros@wantedpixel.com>
2013-12-26 20:46:05 +0100

    added getPhotos in PhotosView
    getPhotos makes search and getSizes Flickr API calls and renders the
    result in the PhotosView

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,5 @@
 var app = app || {};
 
 $(function() {
-    var photos = [
-        { title: 'photo 1' },
-        { title: 'photo 2' },
-        { title: 'photo 3' },
-        { title: 'photo 4' },
-        { title: 'photo 5' }
-    ];
-
     new app.PhotosView( photos );
 });
--- app/js/views/photos.js
+++ app/js/views/photos.js
@@ -2,7 +2,9 @@ var app = app || {};
 
 app.PhotosView = Backbone.View.extend({
     el: '#photos',
-
+    events:{
+        'click #getPhotos':'getPhotos'
+    },
     initialize: function( initialPhotos ) {
         this.collection = new app.Photos( initialPhotos );
         this.render();
@@ -22,5 +24,22 @@ app.PhotosView = Backbone.View.extend({
             model: item
         });
         this.$el.append( photoView.render().el );
+    },
+
+    // get new photos
+    getPhotos: function(e) {
+        e.preventDefault();
+        var thisClass = this;
+        // jQuery get call to photos.search call
+        $.get( "http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=cd30ea58809313bcbe2d7b65482cf48f&per_page=10&nojsoncallback=1&text=polilimnio", function( data ) {
+            data.photos.photo.forEach(function(entry) {
+                // http get to get single photo data
+                $.get( 'http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&api_key=cd30ea58809313bcbe2d7b65482cf48f&nojsoncallback=1&photo_id='+entry.id, function(data) {
+                    // create the new Photo model with the fetched data
+                    // render the photo
+                    thisClass.renderPhoto( new app.Photo({title:entry.title, image: data.sizes.size[0].source}) );
+                });
+            });
+        });
     }
 });
